6b76085627eae6580bb571eb59786894e8e08149
zonzz <88747756+zonzzlive@users.noreply.github.com>
2022-04-03 18:59:34 +0200

    :construction: Project init

---
+++ assets/project.js
@@ -0,0 +1,9 @@
+import '.styles/project.scss';
+
+import './bootstrap';
+
+let projectId = document.querySelector('.my-project').dataset.projectid
+let projectCode = document.querySelector('.my-project-code').dataset.projectid
+
+console.log(projectId);
+console.log(projectCode);
