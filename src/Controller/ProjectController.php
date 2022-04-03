656afd40a797e7a44db76846e84377c033007a07
zonzz <88747756+zonzzlive@users.noreply.github.com>
2022-04-03 20:19:32 +0200

    🔨Project part

---
+++ src/Controller/ProjectController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Project;
+use App\Form\ProjectType;
+use App\Repository\ProjectRepository;
+use App\Repository\HighlightRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/project')]
+class ProjectController extends AbstractController
+{
+    #[Route('/', name: 'app_project', methods: ['GET'])]
+    public function index(ProjectRepository $projectRepository): Response
+    {
+        return $this->render('project/index.html.twig', [
+            'projects' => $projectRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/new', name: 'project_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $project = new Project();
+        $form = $this->createForm(ProjectType::class, $project);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $project->setstatus($form->getData()->getStatus());
+            $entityManager->persist($project);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_homepage', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('project/new.html.twig', [
+            'project' => $project,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'project_show', methods: ['GET'])]
+    public function show(Project $project, HighlightRepository $highlightRepository): Response
+    {
+        return $this->render('project/show.html.twig', [
+            'project' => $project,
+            'highlights' => $highlightRepository->findBy(["project" => $project->getId()]),
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'project_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Project $project, EntityManagerInterface $entityManager): Response
+    {
+        $form = $this->createForm(ProjectType::class, $project);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_homepage', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('project/edit.html.twig', [
+            'project' => $project,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'project_delete', methods: ['POST'])]
+    public function delete(Request $request, Project $project, EntityManagerInterface $entityManager): Response
+    {
+        //dd($project);
+        if ($this->isCsrfTokenValid('delete'.$project->getId(), $request->request->get('_token'))) {
+            $entityManager->remove($project);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('app_homepage', [], Response::HTTP_SEE_OTHER);
+    }
+}
